5c4f8de03e743eb2c98ad1c6d9e69360d616e75b
likecoldbrew <likecoldbrew@gmail.com>
2024-10-02 09:29:37 +0900

    Initial commit

---
+++ backend/src/main/java/kr/or/nextit/backend/Controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package kr.or.nextit.backend.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //Tip : @RestController = @Controller + @ResponseBody
+public class HelloWorldController {
+
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
+    }
+}
