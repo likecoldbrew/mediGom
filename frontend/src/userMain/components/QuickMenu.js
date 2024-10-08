720c6e3725f3d340da589e190d4af7d466d56686
psy0525 <shineeluv525@gmail.com>
2024-10-08 09:24:15 +0900

    챗봇 디자인 수정 검색바 만드는 중

--- frontend/src/userMain/components/QuickMenu.js
+++ frontend/src/userMain/components/QuickMenu.js
@@ -8,8 +8,8 @@ const QuickMenu=()=>{
         { name: "이용안내", icon: "ℹ️" },
     ];
     return (
-        <div className="sticky top-20 right-4 bg-sky-50 rounded-lg p-4 shadow-md h-80">
-            <h3 className="text-lg font-bold text-sky-800 mb-4">빠른 메뉴</h3>
+        <div className="sticky top-10 right-4 bg-sky-50 rounded-lg p-4 shadow-md h-60">
+
             <div className="space-y-4 h-full  overflow-y-auto">
                 {sidebarItems.map((item, index) => (
                     <button
